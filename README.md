3ca5c872748e88e808bba2dfd582745c745de789
Deividas Jasas <jasas.code@gmail.com>
2024-09-28 17:37:46 +0300

    Finished mobile without complex section

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # VanillaJs-project
+
+im using a parcel bundler so i could access all imports from glide library
+
+npm run start to start dev envirominet 
+npm run scss to change styles
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import Glide from '@glidejs/glide';
+
+const pathToSlideImage = [
+  './assests/Ellipse-full.png',
+  './assests/Ellipse-Empty.png',
+];
+
+const glide = new Glide('.glide', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 1,
+  // breakpoints: {
+  //   600: { perView: 1 },
+  //   1200: { perView: 3 }
+  // }
+});
+glide.on('move', (newIndex) => {
+  console.log('Current Slide Index:', newIndex);
+  const trackerOne = document.getElementById('navigationImgOne');
+  const trackerTwo = document.getElementById('navigationImgTwo');
+  const trackerThree = document.getElementById('navigationImgThree');
+
+  if (newIndex.movement === 0) {
+    trackerOne.src = pathToSlideImage[0];
+    trackerTwo.src = pathToSlideImage[1];
+    trackerThree.src = pathToSlideImage[1];
+  } else if(newIndex.movement === 258) {
+    trackerOne.src = pathToSlideImage[1];
+    trackerTwo.src = pathToSlideImage[0];
+    trackerThree.src = pathToSlideImage[1];
+  } else {
+    trackerOne.src = pathToSlideImage[1];
+    trackerTwo.src = pathToSlideImage[1];
+    trackerThree.src = pathToSlideImage[0];
+
+  }
+});
+
+
+
+glide.mount();
